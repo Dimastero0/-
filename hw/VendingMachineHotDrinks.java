6c1fc3abbc7b90d70dc7c2bb7cf1d1bc0a1f6643
Dimastero0 <154535533+Dimastero0@users.noreply.github.com>
2024-06-22 10:31:00 +0300

    Update VendingMachineHotDrinks.java

--- hw/VendingMachineHotDrinks.java
+++ hw/VendingMachineHotDrinks.java
@@ -4,44 +4,27 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Класс автомата горячих напитков
- */
 public class VendingMachineHotDrinks implements VendingMachine {
     List<HotDrinks> hotDrinksList = new ArrayList<>();
     List<HotDrinks> resultFind = new ArrayList<>();
 
-    /**
-     * Переопределенный метод интерфейса VendingMachine
-     */
+   
     @Override
     public void initProduct() {
     }
 
-    /**
-     * Перегруженный метод
-     *
-     * @param list лист объектов класса hotDrinks
-     */
+    
     public void initProduct(List<HotDrinks> list) {
         this.hotDrinksList = list;
     }
 
-    /**
-     * Переопределенный метод
-     *
-     * @return null
-     */
+    
     @Override
     public String getProduct() {
         return null;
     }
 
-    /**
-     * Перегруженный метод получения горячего напитка по названию
-     *
-     * @return Список найденных экземпляров класса HotDrinks
-     */
+
     public String getProduct(String name) {
         resultFind.clear();
         for (HotDrinks el : hotDrinksList) {
@@ -50,11 +33,7 @@ public class VendingMachineHotDrinks implements VendingMachine {
         return resultFind.toString();
     }
 
-    /**
-     * Перегруженный метод получения горячего напитка по цене (меньшей или равной)
-     *
-     * @return Список найденных экземпляров класса HotDrinks
-     */
+   
     public String getProduct(BigDecimal price) {
         resultFind.clear();
         for (HotDrinks el : hotDrinksList) {
@@ -63,11 +42,7 @@ public class VendingMachineHotDrinks implements VendingMachine {
         return resultFind.toString();
     }
 
-    /**
-     * Перегруженный метод получения горячего напитка по цене (меньшей или равной)
-     *
-     * @return Список найденных экземпляров класса HotDrinks
-     */
+   
     public String getProduct(int temp) {
         resultFind.clear();
         for (HotDrinks el : hotDrinksList) {
